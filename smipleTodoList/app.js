4218e004c6016c041e94b5c6efd006b5b533d3a3
anyu8242 <anyu8242@gmail.com>
2024-07-29 17:13:15 +0800

    delBtnFix

--- smipleTodoList/app.js
+++ smipleTodoList/app.js
@@ -10,8 +10,8 @@ addList.addEventListener("click", e => {
     let form = e.target.parentElement;
     console.log(form.children) //預期會找到表單裡的子元素，HTMLCollection
     let toDoText = form.children[0].value;
-    let toDoM = form.children[1].value;
-    let toDoD = form.children[2].value;
+    let toDoM = form.children[2].value;
+    let toDoD = form.children[4].value;
     // console.log(toDoText, toDoM, toDoD); //內容就是子元素
 
     // 把取得的資料放入section << 練習點
@@ -33,14 +33,19 @@ addList.addEventListener("click", e => {
     doneBtn.innerHTML = `<span class="material-symbols-outlined">check_small</span>`;
     doneBtn.classList.add("doneBtn");
     doneBtn.addEventListener("click", e => {
-        console.log(e.target);//有時會按到按鈕有時會按到icon，所以在css的時候要把按到icon的動作取消
-        console.log(e.target.parentElement)
+        // console.log(e.target);//有時會按到按鈕有時會按到icon，所以在css的時候要把按到icon的動作取消
+        // console.log(e.target.parentElement)
         let todoItem = e.target.parentElement; // 點下按鈕要讓整個todo作用
-        todoItem.classList.add("done");
+        todoItem.classList.toggle("done");
     })
     let delBtn = document.createElement("button");
     delBtn.innerHTML = `<span class="material-symbols-outlined">delete</span>`
     delBtn.classList.add("delBtn")
+    delBtn.addEventListener("click", e => {
+        // console.log(e.target.parentElement);
+        let todoItem = e.target.parentElement;
+        todoItem.remove();
+    })
     toDo.appendChild(doneBtn);
     toDo.appendChild(delBtn);
 
